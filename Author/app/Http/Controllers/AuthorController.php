591cb92597cbfe77ad14e7f5db7c90b55a730161
tqt97 <kutuanonline199@gmail.com>
2024-04-06 19:42:08 +0700

    completed AuthorController

--- Author/app/Http/Controllers/AuthorController.php
+++ Author/app/Http/Controllers/AuthorController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Models\Author;
 use App\Traits\ApiResponse;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class AuthorController extends Controller
 {
@@ -41,15 +43,42 @@ class AuthorController extends Controller
         return $this->success($author, Response::HTTP_CREATED);
     }
 
-    public function show(Author $author)
+    public function show(int $id)
     {
+        $author = Author::findOrFail($id);
+
+        return $this->success($author);
     }
 
-    public function update(Request $request, Author $author)
+    public function update(Request $request, int $id)
     {
+        $rules = [
+            'name' => 'required|max:255',
+            'gender' => 'required|in:male,female',
+            'country' => 'required|max:255',
+        ];
+        $author = Author::findOrFail($id);
+
+        $this->validate($request, $rules);
+        $author->fill($request->all());
+        if ($author->isClean()) {
+            return $this->error('Nothing to update', Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+        
+        if ($author->save()) {
+            return $this->success($author);
+        }
+        return $this->error('Something went wrong', Response::HTTP_INTERNAL_SERVER_ERROR);
     }
 
-    public function destroy(Author $author)
+    public function destroy(int $id)
     {
+        $author = Author::findOrFail($id);
+        try {
+            $author->delete();
+            return $this->success($author->name . ' has been deleted');
+        } catch (Exception $exception) {
+            return $this->error('Something went wrong', Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 }
--- Author/routes/web.php
+++ Author/routes/web.php
@@ -19,7 +19,7 @@
 
 $router->get('/authors', 'AuthorController@index');
 $router->post('/authors', 'AuthorController@store');
-$router->get('/authors/{author}', 'AuthorController@show');
-$router->put('/authors/{author}', 'AuthorController@update');
-$router->patch('/authors/{author}', 'AuthorController@update');
-$router->delete('/authors/{author}', 'AuthorController@destroy');
+$router->get('/authors/{id}', 'AuthorController@show');
+$router->put('/authors/{id}', 'AuthorController@update');
+$router->patch('/authors/{id}', 'AuthorController@update');
+$router->delete('/authors/{id}', 'AuthorController@destroy');
